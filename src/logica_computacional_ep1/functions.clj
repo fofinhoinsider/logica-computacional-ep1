0c33d923ba071ebcaaef4d4cf4052fc2ae47e342
fofinhoinsider <andrei.sgi97@gmail.com>
2023-02-12 18:03:45 -0300

    transitive logic

--- src/logica_computacional_ep1/functions.clj
+++ src/logica_computacional_ep1/functions.clj
@@ -6,16 +6,16 @@
   "Takes file name and reads data."
   [fname]
   (with-open [reader (io/reader fname)]
-  (doall
-    (csv/read-csv reader))))
+    (doall
+     (csv/read-csv reader))))
 
 (defn insert-value
   [matrix line column value]
   (assoc-in (vec matrix) [line column] value))
 
-(defn get-reflexive 
+(defn get-reflexive
   ([matrix]
-   (get-reflexive matrix 0)) 
+   (get-reflexive matrix 0))
   ([matrix index]
    (if (= index (count matrix))
      matrix
@@ -24,22 +24,34 @@
 (defn write-to-csv-file
   [matrix fname]
   (with-open [writer (io/writer fname)]
-    (csv/write-csv writer matrix))
-)
+    (csv/write-csv writer matrix)))
+
+(defn get-indexes-of-true-values
+  [matrix]
+  (map (fn [[line column _]] [line column])
+       (filter #(= 1 (nth % 2))
+               (reduce
+                (fn [list indexed-line]
+                  (concat
+                   list
+                   (map-indexed
+                    (fn [idx item] [(nth indexed-line 0) idx item])
+                    (nth indexed-line 1))))
+                ()
+                (map-indexed vector matrix)))))
+
 
 (defn get-transitive
-  [matrix]
-  ;; (map-indexed vector (map-indexed vector matrix))
-  ;; (map-indexed vector matrix)
-  (let [list-of-indexes []]
-  (for [i (range (count matrix))]
-    (for [j (range (count (nth matrix i)))]
-      (if true
-        (println "True")
-        (conj! list-of-indexes [i j])
-      )
-    )
-  )
-  (println list-of-indexes)
-  )
-)
+  ([matrix]
+   (get-transitive matrix (get-indexes-of-true-values matrix) 0 1))
+  ([matrix indexes i j]
+   (if (= i (count indexes))
+     matrix
+     (if (= (nth (nth indexes i) 1) (nth (nth indexes j) 0))
+       (get-transitive (insert-value
+                        matrix
+                        (nth (nth indexes i) 0)
+                        (nth (nth indexes j) 1)
+                        1) indexes i (inc j))
+       ()))))
+
